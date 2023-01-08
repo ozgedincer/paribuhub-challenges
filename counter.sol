c6a9a6ec1ffca8ef925ce64f8112418e2a4628fa
Özge Dinçer <dncerozge@gmail.com>
2023-01-08 18:11:28 +0300

    Create counter.sol

---
+++ counter.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.8.17;
+
+contract Counter {
+    uint public count;
+
+    function get() public view returns (uint) {
+        return count;
+    }
+
+    function inc() public {
+        count += 1;
+    }
+
+    function dec() public {
+        // This function will fail if count = 0
+        count -= 1;
+    }
+}
